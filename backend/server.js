51c3953e0b6723ab32fd6f869b1bbc3fa29ca6c2
abbudy <abdurrehmanahmed0182@gmail.com>
2025-04-23 17:43:50 +0300

    Bazario project

---
+++ backend/server.js
@@ -0,0 +1,10 @@
+import express from "express";
+const express = require("express");
+
+const app = express();
+app.get("/",(req,res) =>{
+    res.send("server is ready");
+});
+app.listen(5000,() => {
+    console.log("server started at http://localhost:5000/")
+});
